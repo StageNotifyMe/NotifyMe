8d81a761604b0d21a3e78e8c5d75567cf105e15d
Arthur De Craemer <arthur.decraemer@student.kdg.be>
2021-04-19 15:41:59 +0200

    multi module project

---
+++ main2/src/test/java/be/xplore/notifyme/NotifymeApplicationTests.java
@@ -0,0 +1,17 @@
+package be.xplore.notifyme;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class NotifymeApplicationTests {
+
+  @Test
+  void contextLoads() {
+    assertEquals(1, 1);
+  }
+
+}
