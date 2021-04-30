ca0677ea88e250404bee9954707eba4b2079ef6b
timschelpexplore <Tim.Schelpe@xplore.be>
2021-04-30 15:32:33 +0200

    removed 2 code smells

--- main2/src/main/java/be/xplore/notifyme/domain/Event.java
+++ main2/src/main/java/be/xplore/notifyme/domain/Event.java
@@ -1,6 +1,5 @@
 package be.xplore.notifyme.domain;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -13,7 +12,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Getter
