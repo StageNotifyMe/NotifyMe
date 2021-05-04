a88b5c28411aabdf75b478667ae299e0eee0863c
timschelpexplore <Tim.Schelpe@xplore.be>
2021-05-04 15:18:03 +0200

    wrote 3 tests in eventService

--- main2/src/test/java/be/xplore/notifyme/service/EventServiceTest.java
+++ main2/src/test/java/be/xplore/notifyme/service/EventServiceTest.java
@@ -2,21 +2,26 @@ package be.xplore.notifyme.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 import be.xplore.notifyme.domain.Address;
+import be.xplore.notifyme.domain.Event;
 import be.xplore.notifyme.domain.User;
 import be.xplore.notifyme.domain.Venue;
 import be.xplore.notifyme.dto.CreateEventDto;
 import be.xplore.notifyme.exception.CrudException;
+import be.xplore.notifyme.exception.UnauthorizedException;
 import be.xplore.notifyme.persistence.IEventRepo;
 import be.xplore.notifyme.persistence.IVenueRepo;
 import java.security.Principal;
 import java.util.LinkedList;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.keycloak.representations.IDToken;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -33,11 +38,11 @@ class EventServiceTest {
   @MockBean
   private UserService userService;
   @MockBean
+  private TokenService tokenService;
+  @MockBean
   private IEventRepo eventRepo;
   @MockBean
   private IVenueRepo venueRepo;
-
-  private final User testUser = new User();
 
   @Test
   void createEventSuccessful() {
@@ -60,6 +65,57 @@ class EventServiceTest {
         eventService.createEvent(ced, principal));
   }
 
+  @Test
+  void getEventSuccessful() {
+    when(eventRepo.findById(1L)).thenReturn(Optional.of(testEvent));
+
+    assertEquals(testEvent.getId(), eventService.getEvent(1L).getId());
+  }
+
+  @Test
+  void getEventNotFound() {
+    when(eventRepo.findById(1L)).thenReturn(Optional.empty());
+
+    assertThrows(CrudException.class, () -> {
+      eventService.getEvent(1L);
+    });
+  }
+
+  @Test
+  void getEventAndVerifyLineManagerPermissionSuccessful() {
+    final KeycloakAuthenticationToken principal = Mockito.mock(KeycloakAuthenticationToken.class);
+    final IDToken token = Mockito.mock(IDToken.class);
+    when(eventRepo.findById(1L)).thenReturn(Optional.of(getTestEventWithLineManager()));
+    when(tokenService.getIdToken(any(Principal.class))).thenReturn(token);
+    when(token.getPreferredUsername()).thenReturn("testUser");
+    when(userService.getUser("testUser")).thenReturn(testUser);
+    when(testUser.getUserId()).thenReturn("testUser");
+
+    var result = eventService.getEventAndVerifyLineManagerPermission(1L, principal);
+    assertEquals(getTestEventWithLineManager().getId(), result.getId());
+    assertTrue(
+        result.getLineManagers().stream()
+            .anyMatch(user -> user.getUserId().equals(testUser.getUserId())));
+  }
+
+  @Test
+  void getEventAndVerifyLineManagerPermissionUnauthorized() {
+    final KeycloakAuthenticationToken principal = Mockito.mock(KeycloakAuthenticationToken.class);
+    final IDToken token = Mockito.mock(IDToken.class);
+    testEvent.setLineManagers(new LinkedList<>());
+    when(eventRepo.findById(1L)).thenReturn(Optional.of(testEvent));
+    when(tokenService.getIdToken(any(Principal.class))).thenReturn(token);
+    when(token.getPreferredUsername()).thenReturn("testUser");
+    when(userService.getUser("testUser")).thenReturn(testUser);
+    when(testUser.getUserId()).thenReturn("testUser");
+
+    assertThrows(UnauthorizedException.class, () -> {
+      eventService.getEventAndVerifyLineManagerPermission(1L, principal);
+    });
+
+  }
+
+
   private CreateEventDto getTestCreateEventDto() {
     return new CreateEventDto("Evenement", "een evenement", "een artiest",
         "2021-04-30 06:45", 1);
@@ -70,4 +126,16 @@ class EventServiceTest {
     return new Venue(1, "Zaal", "een zaal", address, new LinkedList<>());
   }
 
+  private User testUser = Mockito.mock(User.class);
+
+  private final Event testEvent =
+      new Event("Evenement", "een evenement", "een artiest", "2021-04-30 06:45", getTestVenue());
+
+  private Event getTestEventWithLineManager() {
+    var testEvent =
+        new Event("Evenement", "een evenement", "een artiest", "2021-04-30 06:45", getTestVenue());
+    testEvent.setLineManagers(new LinkedList<>());
+    testEvent.getLineManagers().add(testUser);
+    return testEvent;
+  }
 }
