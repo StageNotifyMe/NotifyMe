e84903e48244ae31f4935ab3f4eb70bde4e776b0
timschelpexplore <Tim.Schelpe@xplore.be>
2021-05-11 11:42:30 +0200

    added save and findById methods to team, user and venue

--- JpaAdapter/src/main/java/be/xplore/notifyme/jpaAdapters/JpaTeamAdapter.java
+++ JpaAdapter/src/main/java/be/xplore/notifyme/jpaAdapters/JpaTeamAdapter.java
@@ -1,7 +1,10 @@
 package be.xplore.notifyme.jpaAdapters;
 
+import be.xplore.notifyme.domain.Team;
+import be.xplore.notifyme.jpaObjects.JpaTeam;
 import be.xplore.notifyme.jpaRepositories.JpaTeamRepository;
 import be.xplore.notifyme.persistence.ITeamRepo;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,14 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class JpaTeamAdapter implements ITeamRepo {
   private final JpaTeamRepository jpaTeamRepository;
+
+  @Override
+  public Team save(Team team) {
+    return jpaTeamRepository.save(new JpaTeam(team)).toDomain();
+  }
+
+  @Override
+  public Optional<Team> findById(long teamId) {
+    return jpaTeamRepository.findById(teamId).map(JpaTeam::toDomain);
+  }
 }
--- domain/src/main/java/be/xplore/notifyme/persistence/IUserRepo.java
+++ domain/src/main/java/be/xplore/notifyme/persistence/IUserRepo.java
@@ -1,9 +1,12 @@
 package be.xplore.notifyme.persistence;
 
 import be.xplore.notifyme.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IUserRepo{
+  User save(User user);
+  Optional<User> findById(String userId);
 }
