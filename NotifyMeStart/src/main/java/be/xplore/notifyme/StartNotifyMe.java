8821d012d3aacaf3fddd08fb3a6207c33af39ac5
Arthur De Craemer <arthur.decraemer@student.kdg.be>
2021-05-11 14:38:06 +0200

    works I guess?

---
+++ NotifyMeStart/src/main/java/be/xplore/notifyme/StartNotifyMe.java
@@ -0,0 +1,16 @@
+package be.xplore.notifyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Starts the notify me application.
+ */
+@SpringBootApplication
+public class StartNotifyMe {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StartNotifyMe.class, args);
+  }
+
+}
