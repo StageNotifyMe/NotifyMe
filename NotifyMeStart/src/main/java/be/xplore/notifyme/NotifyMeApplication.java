7b7017bf97efaa49561dac596ef50f0fc5f1cc09
timschelpexplore <Tim.Schelpe@xplore.be>
2021-05-11 13:26:11 +0200

    module push

--- NotifyMeStart/src/main/java/be/xplore/notifyme/NotifyMeApplication.java
+++
@@ -1,8 +0,0 @@
-package be.xplore.notifyme;
-
-@SpringBootApplication
-public class NotifyMeApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(NotifymeApplication.class, args);
-  }
-}
