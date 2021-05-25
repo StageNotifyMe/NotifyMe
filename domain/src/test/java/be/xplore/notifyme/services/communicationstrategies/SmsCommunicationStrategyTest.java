7336818b3ac792e2b7a875e7fbd2396683799046
timschelpexplore <Tim.Schelpe@xplore.be>
2021-05-25 11:14:39 +0200

    added test methods for smsCommunicationStrategy

--- domain/src/test/java/be/xplore/notifyme/services/communicationstrategies/SmsCommunicationStrategyTest.java
+++ domain/src/test/java/be/xplore/notifyme/services/communicationstrategies/SmsCommunicationStrategyTest.java
@@ -1,29 +1,28 @@
 package be.xplore.notifyme.services.communicationstrategies;
 
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest(classes = {EmailCommunicationStrategy.class})
-class EmailCommunicationStrategyTest {
+@SpringBootTest(classes = {SmsCommunicationStrategy.class})
+class SmsCommunicationStrategyTest {
   @Autowired
-  private EmailCommunicationStrategy emailCommunicationStrategy;
+  private SmsCommunicationStrategy smsCommunicationStrategy;
   @MockBean
-  private IEmailService emailService;
+  private ISmsService smsService;
 
   @Test
   void getName() {
-    assertEquals("Email", emailCommunicationStrategy.getName());
+    assertEquals("SMS", smsCommunicationStrategy.getName());
   }
 
   @Test
-  void getEmailService() {
-    assertThat(emailCommunicationStrategy.getEmailService(), instanceOf(IEmailService.class));
+  void getSmsService() {
+    assertThat(smsCommunicationStrategy.getSmsService(), instanceOf(ISmsService.class));
   }
 }
