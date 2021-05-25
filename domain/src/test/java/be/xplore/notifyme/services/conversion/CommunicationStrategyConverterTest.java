98d93c2688b038e48351f8415ac28b51141cf599
timschelpexplore <Tim.Schelpe@xplore.be>
2021-05-25 10:51:19 +0200

    added tests for communicationStrategyConverter

---
+++ domain/src/test/java/be/xplore/notifyme/services/conversion/CommunicationStrategyConverterTest.java
@@ -0,0 +1,43 @@
+package be.xplore.notifyme.services.conversion;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.jupiter.api.Assertions.*;
+
+import be.xplore.notifyme.services.communicationstrategies.EmailCommunicationStrategy;
+import be.xplore.notifyme.services.communicationstrategies.IEmailService;
+import be.xplore.notifyme.services.communicationstrategies.ISmsService;
+import be.xplore.notifyme.services.communicationstrategies.SmsCommunicationStrategy;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest(classes = {CommunicationStrategyConverter.class})
+class CommunicationStrategyConverterTest {
+  @Autowired
+  private CommunicationStrategyConverter communicationStrategyConverter;
+  @MockBean
+  private IEmailService emailService;
+  @MockBean
+  private ISmsService smsService;
+
+  @Test
+  void convertToDatabaseColumn() {
+    assertEquals("emailcommunicationstrategy", communicationStrategyConverter
+        .convertToDatabaseColumn(new EmailCommunicationStrategy(null)));
+    assertEquals("smscommunicationstrategy",
+        communicationStrategyConverter.convertToDatabaseColumn(new SmsCommunicationStrategy(null)));
+  }
+
+  @Test
+  void convertToEntityAttribute() {
+    assertThat(
+        communicationStrategyConverter.convertToEntityAttribute("emailcommunicationstrategy"),
+        instanceOf(EmailCommunicationStrategy.class));
+    assertThat(
+        communicationStrategyConverter.convertToEntityAttribute("smscommunicationstrategy"),
+        instanceOf(SmsCommunicationStrategy.class));
+    assertNull(communicationStrategyConverter.convertToEntityAttribute("invalid"));
+  }
+}
