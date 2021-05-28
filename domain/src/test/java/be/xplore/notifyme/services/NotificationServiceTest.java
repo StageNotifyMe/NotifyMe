b468edc8c6ac1ee0389b197659c4da8586299fc7
timschelpexplore <Tim.Schelpe@xplore.be>
2021-05-28 09:45:15 +0200

    wrote 2 tests for notificationService

--- domain/src/test/java/be/xplore/notifyme/services/NotificationServiceTest.java
+++ domain/src/test/java/be/xplore/notifyme/services/NotificationServiceTest.java
@@ -2,22 +2,29 @@ package be.xplore.notifyme.services;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import be.xplore.notifyme.domain.CommunicationPreference;
+import be.xplore.notifyme.domain.Event;
+import be.xplore.notifyme.domain.EventStatus;
 import be.xplore.notifyme.domain.Message;
 import be.xplore.notifyme.domain.Notification;
 import be.xplore.notifyme.domain.User;
+import be.xplore.notifyme.domain.Venue;
 import be.xplore.notifyme.persistence.IMessageRepo;
 import be.xplore.notifyme.persistence.INotificationRepo;
 import be.xplore.notifyme.services.communicationstrategies.EmailCommunicationStrategy;
 import be.xplore.notifyme.services.communicationstrategies.IEmailService;
 import be.xplore.notifyme.services.communicationstrategies.ISmsService;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import org.junit.jupiter.api.Test;
 import org.keycloak.representations.account.UserRepresentation;
 import org.mockito.invocation.InvocationOnMock;
@@ -125,5 +132,103 @@ class NotificationServiceTest {
     userRep.setId("userId");
     userRep.setEmailVerified(true);
     return userRep;
+  }
+
+  @Test
+  void createCanceledEventMessage() {
+    var event = new Event(1L, "title", "description", "artist", LocalDateTime.now(), EventStatus.OK,
+        new Venue(), new ArrayList<>(), new HashSet<>());
+    when(messageRepo.save(any())).thenAnswer(new Answer<Message>() {
+      @Override
+      public Message answer(InvocationOnMock invocation) throws Throwable {
+        var args = invocation.getArguments();
+        return (Message) args[0];
+      }
+    });
+
+    var message = notificationService.createCanceledEventMessage(event);
+    assertTrue(message.getText().contains("EventId: 1"));
+    assertTrue(message.getText().contains("Title: title"));
+    assertTrue(message.getText().contains("Description: description"));
+  }
+
+  @Test
+  void notifyOrganisationsManagers() {
+    mockNotifyOrgManagers();
+
+    var orgIdList = new ArrayList<Long>();
+    orgIdList.add(1L);
+    orgIdList.add(2L);
+    assertDoesNotThrow(() -> {
+      notificationService.notifyOrganisationsManagers(orgIdList, 1L);
+    });
+  }
+
+  private void mockNotifyOrgManagers() {
+    var orgManager1 = new User("org1-man", "orgMan1");
+    var orgManager2 = new User("org2-man", "orgMan2");
+    this.mockGetOrganisationManagers(orgManager1, orgManager2);
+    var message = new Message(1L, "title", "text");
+    var comPref = new CommunicationPreference(1L, orgManager1, true, true, true,
+        mockEmailCommunicationStrategy());
+    this.mockCreateNotification(message, orgManager1, comPref);
+    this.mockCreateNotification(message, orgManager2, comPref);
+    this.mockGetUserInfoUsername();
+    this.mockSave();
+  }
+
+  private EmailCommunicationStrategy mockEmailCommunicationStrategy() {
+    var emailComStrat = mock(EmailCommunicationStrategy.class);
+    doNothing().when(emailComStrat).send(any());
+    return emailComStrat;
+  }
+
+
+  private void mockGetOrganisationManagers(User orgManager1, User orgManager2) {
+    var org1managers = new ArrayList<User>();
+    org1managers.add(orgManager1);
+    var org2managers = new ArrayList<User>();
+    org2managers.add(orgManager2);
+    when(organisationService.getOrganisationManagers(1L)).thenReturn(org1managers);
+    when(organisationService.getOrganisationManagers(2L)).thenReturn(org2managers);
+  }
+
+  private void mockCreateNotification(Message message, User user, CommunicationPreference comPref) {
+    var notification =
+        new Notification(1L, "address", comPref, "emailservice", message,
+            user);
+    when(notificationRepo.create(anyLong(), anyString())).thenReturn(notification);
+  }
+
+  private void mockGetUserInfoUsername() {
+    when(keycloakCommunicationService.getUserInfoUsername(anyString())).thenAnswer(
+        new Answer<UserRepresentation>() {
+          @Override
+          public UserRepresentation answer(InvocationOnMock invocation) throws Throwable {
+            var args = invocation.getArguments();
+            var userRep = getDummyUserRep();
+            userRep.setUsername((String) args[0]);
+            return userRep;
+          }
+        });
+  }
+
+  private void mockSave() {
+    when(notificationRepo.save(any())).thenAnswer(new Answer<Notification>() {
+      @Override
+      public Notification answer(InvocationOnMock invocation) throws Throwable {
+        return invocation.getArgument(0);
+      }
+    });
+    when(messageRepo.save(any())).thenAnswer(new Answer<Message>() {
+      @Override
+      public Message answer(InvocationOnMock invocation) throws Throwable {
+        return invocation.getArgument(0);
+      }
+    });
+  }
+
+  @Test
+  void notifyUsers() {
   }
 }
