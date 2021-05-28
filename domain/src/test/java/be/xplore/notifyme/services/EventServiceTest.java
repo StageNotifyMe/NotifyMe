eab599677cb2bd8f200150d72034f43ad38202bc
timschelpexplore <Tim.Schelpe@xplore.be>
2021-05-28 11:02:35 +0200

    eventService tested

--- domain/src/test/java/be/xplore/notifyme/services/EventServiceTest.java
+++ domain/src/test/java/be/xplore/notifyme/services/EventServiceTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
@@ -16,6 +17,7 @@ import static org.mockito.Mockito.when;
 import be.xplore.notifyme.domain.Address;
 import be.xplore.notifyme.domain.Event;
 import be.xplore.notifyme.domain.EventStatus;
+import be.xplore.notifyme.domain.Message;
 import be.xplore.notifyme.domain.User;
 import be.xplore.notifyme.domain.Venue;
 import be.xplore.notifyme.dto.CreateEventDto;
@@ -23,9 +25,11 @@ import be.xplore.notifyme.exception.CrudException;
 import be.xplore.notifyme.exception.SaveToDatabaseException;
 import be.xplore.notifyme.exception.UnauthorizedException;
 import be.xplore.notifyme.persistence.IEventRepo;
+import be.xplore.notifyme.persistence.IMessageRepo;
 import be.xplore.notifyme.persistence.IVenueRepo;
 import java.security.Principal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -34,6 +38,8 @@ import org.junit.jupiter.api.Test;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.keycloak.representations.IDToken;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -55,6 +61,8 @@ class EventServiceTest {
   private IEventRepo eventRepo;
   @MockBean
   private IVenueRepo venueRepo;
+  @MockBean
+  private IMessageRepo messageRepo;
 
   @Test
   void createEventSuccessful() {
@@ -214,5 +222,55 @@ class EventServiceTest {
     testEvent.setLineManagers(new HashSet<>());
     testEvent.getLineManagers().add(testUser);
     return testEvent;
+  }
+
+  @Test
+  void updateEventStatus() {
+    mockUpdateEventStatus();
+
+    var okEvent = eventService.updateEventStatus(1L, EventStatus.OK);
+    assertEquals(EventStatus.OK, okEvent.getEventStatus());
+
+    var cancelEvent = eventService.updateEventStatus(2L, EventStatus.CANCELED);
+    assertEquals(EventStatus.CANCELED, cancelEvent.getEventStatus());
+  }
+
+  private void mockUpdateEventStatus() {
+    when(eventRepo.updateEventStatus(anyLong(), any())).thenAnswer(new Answer<Event>() {
+      @Override
+      public Event answer(InvocationOnMock invocation) throws Throwable {
+        var args = invocation.getArguments();
+        return new Event((long) args[0], "title", "description", "artist", LocalDateTime.now(),
+            (EventStatus) args[1], new Venue(), new LinkedList<>(), new HashSet<>());
+      }
+    });
+    when(notificationService.createCanceledEventMessage(any())).thenAnswer(new Answer<Message>() {
+      @Override
+      public Message answer(InvocationOnMock invocation) throws Throwable {
+        var args = invocation.getArguments();
+        return new Message(1L, String.format("event %d canceled", ((Event) args[0]).getId()),
+            "text");
+      }
+    });
+    this.mockGetAllOrganisationIds(2);
+    doNothing().when(notificationService).notifyOrganisationsManagers(anyList(), anyLong());
+    this.mockGetAttendingMembers(4);
+    doNothing().when(notificationService).notifyUsers(anyList(), anyLong());
+  }
+
+  private void mockGetAllOrganisationIds(long amount) {
+    var orgIds = new ArrayList<Long>();
+    for (long i = 1L; i <= amount; i++) {
+      orgIds.add(i);
+    }
+    when(eventRepo.getAllOrganisationIds(anyLong())).thenReturn(orgIds);
+  }
+
+  private void mockGetAttendingMembers(int amount) {
+    var attendingMembers = new ArrayList<User>();
+    for (int i = 0; i < amount; i++) {
+      attendingMembers.add(new User(String.format("userId%s", i), String.format("username%s", i)));
+    }
+    when(eventRepo.getAttendingMembers(anyLong())).thenReturn(attendingMembers);
   }
 }
