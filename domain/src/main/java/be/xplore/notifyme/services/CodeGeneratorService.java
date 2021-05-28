ef10b019791f18ce4fc99530c71489e6ac5bedf6
Arthur De Craemer <arthur.decraemer@student.kdg.be>
2021-05-28 14:17:48 +0200

    secured random generation

--- domain/src/main/java/be/xplore/notifyme/services/CodeGeneratorService.java
+++ domain/src/main/java/be/xplore/notifyme/services/CodeGeneratorService.java
@@ -1,7 +1,8 @@
 package be.xplore.notifyme.services;
 
-import java.util.Random;
+import java.security.SecureRandom;
 import lombok.RequiredArgsConstructor;
+import org.keycloak.common.util.RandomString;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -14,14 +15,8 @@ public class CodeGeneratorService {
    * @return random alphanumeric string. 0-9 & a-z.
    */
   public String generatePhoneVerificationCode() {
-    var leftLimit = 48; // numeral '0'
-    var rightLimit = 122; //letter z
-    var targetStringLength = 10;
-
-    return new Random().ints(leftLimit, rightLimit + 1)
-        .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-        .limit(targetStringLength)
-        .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-        .toString();
+    var alphanum = RandomString.alphanum;
+    var verificationCode = new RandomString(10, new SecureRandom(), alphanum);
+    return verificationCode.nextString();
   }
 }
