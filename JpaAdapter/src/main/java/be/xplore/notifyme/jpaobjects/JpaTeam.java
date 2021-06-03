75cf02936a99564e9747a1685141ebfd415d19e7
timschelpexplore <Tim.Schelpe@xplore.be>
2021-06-03 16:42:48 +0200

    fixed jpa mapping error

--- JpaAdapter/src/main/java/be/xplore/notifyme/jpaobjects/JpaTeam.java
+++ JpaAdapter/src/main/java/be/xplore/notifyme/jpaobjects/JpaTeam.java
@@ -30,7 +30,7 @@ public class JpaTeam {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
-  @OneToOne
+  @OneToOne(mappedBy = "team")
   private JpaLine line;
   @ManyToMany
   private List<JpaOrganisation> organisations;
