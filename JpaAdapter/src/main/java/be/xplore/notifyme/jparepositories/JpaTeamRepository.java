a3b4941c17e306450640fa8608fb25247237e6c4
timschelpexplore <Tim.Schelpe@xplore.be>
2021-06-03 17:07:09 +0200

    mapping hotfix 2

--- JpaAdapter/src/main/java/be/xplore/notifyme/jparepositories/JpaTeamRepository.java
+++ JpaAdapter/src/main/java/be/xplore/notifyme/jparepositories/JpaTeamRepository.java
@@ -1,12 +1,13 @@
 package be.xplore.notifyme.jparepositories;
 
-import be.xplore.notifyme.domain.TeamApplication;
 import be.xplore.notifyme.jpaobjects.JpaTeam;
 import be.xplore.notifyme.jpaobjects.JpaTeamApplication;
 import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface JpaTeamRepository extends JpaRepository<JpaTeam, Long> {
@@ -19,4 +20,9 @@ public interface JpaTeamRepository extends JpaRepository<JpaTeam, Long> {
       + "         JOIN organisationUsers.user u"
       + " WHERE u.userId = :orgManagerId")
   Set<JpaTeamApplication> getTeamApplicationsForOrgManager(String orgManagerId);
+
+  @Modifying
+  @Transactional
+  @Query(nativeQuery = true, value = "UPDATE jpa_team SET line_id = :lineId WHERE id = :teamId")
+  void updateTeamLineMapping(long lineId, long teamId);
 }
